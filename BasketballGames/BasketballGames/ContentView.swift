2ac02febfe00f10a755bcfd22a41bd5715380f94
bbccCT <bbcc@unc.edu>
2025-03-02 22:48:41 -0500

    Add code for basketball games app; xcode still refuses to preview

--- BasketballGames/BasketballGames/ContentView.swift
+++ BasketballGames/BasketballGames/ContentView.swift
@@ -7,15 +7,62 @@
 
 import SwiftUI
 
+struct Response: Codable {
+    var results: [Result]
+}
+
+struct Score: Codable {
+    var unc: Int
+    var opponent: Int
+}
+
+struct Result: Codable {
+    var id: Int
+    var team: String
+    var opponent: String
+    var date: String
+    var score: Score
+    var isHomeGame: Bool
+}
+
 struct ContentView: View {
+    @State private var results = [Result]()
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        List(results, id: \.id) { item in
+            VStack(alignment: .leading) {
+                HStack {
+                    Text("\(item.team) vs. \(item.opponent)")
+                    Spacer()
+                    Text("\(item.score.unc) - \(item.score.opponent)")
+                }
+                HStack {
+                    Text("\(item.date)")
+                    Spacer()
+                    Text("\(item.isHomeGame ? "Home" : "Away")")
+                }.font(.caption)
+            }
         }
-        .padding()
+        .task {
+            await loadData()
+        }
+    }
+
+    func loadData() async {
+        guard let url = URL(string: "https://api.samuelshi.com/uncbasketball") else {
+            print("Invalid URL")
+            return
+        }
+
+        do {
+            let (data, _) = try await URLSession.shared.data(from: url)
+
+            if let decodedResponse = try? JSONDecoder().decode(Response.self, from: data) {
+                results = decodedResponse.results
+            }
+        } catch {
+            print("Invalid data")
+        }
     }
 }
 
